8d5ac92a1e7ae0e4e537145e225c2597e492f9fa
Agung Maulana <iamaul@hotmail.com>
2022-02-07 23:19:23 +0700

    Update Summary.tsx

--- src/components/header/Summary.tsx
+++ src/components/header/Summary.tsx
@@ -23,7 +23,7 @@ const Summary: React.FC = () => {
                         boxSize="120px"
                         objectFit="cover"
                         style={{ borderRadius: '3px' }}
-                        src="https://cdn.discordapp.com/avatars/676575693072498698/faf0b6f7d6ecd3296e1eb5292a8117df.png?size=1024"
+                        src="/IMG_0855.HEIC"
                         alt="profile-picture"
                     />
                 </Flex>   
